52f3d309ec2f82b1a7ee15c82e41a0a3bce43a8f
TheEmber <petrusenko2004r@gmail.com>
2023-11-24 22:58:15 +0200

    Removed humaninfecable, added methods to disease

--- Content.Server/Virology/DiseaseComponents.cs
+++ Content.Server/Virology/DiseaseComponents.cs
@@ -13,14 +13,26 @@ public sealed partial class DiseaseComponent : Component
     [DataField]
     public Disease[] Diseases { get; set; } = new Disease[0];
     [DataField]
-    public Disease[] DisseasesImmunity { get; set; } = new Disease[0];
-    public DiseaseComponent(Disease disease)
-    {
-        Diseases.Append(disease);
-    }
-    public DiseaseComponent()
-    {
+    public Disease[] DiseasesImmunity { get; set; } = new Disease[0];
 
+    public void AddDisease(Disease newDisease)
+    {
+        Diseases.Append(newDisease);
+    }
+
+    public void AddDisease(Disease[] newDiseases)
+    {
+        Diseases = Diseases.Concat(newDiseases).ToArray();
+    }
+
+    public void AddDiseaseImmunity(Disease newImmunity)
+    {
+        DiseasesImmunity.Append(newImmunity);
+    }
+
+    public void AddDiseaseImmunity(Disease[] newImmunities)
+    {
+        DiseasesImmunity = DiseasesImmunity.Concat(newImmunities).ToArray();
     }
 }
 
